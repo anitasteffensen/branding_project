358531ed54c343d3143279d512b0d011c711cb51
AnitaSteffensen <anitanyholms@gmail.com>
2024-02-29 17:19:38 +0100

    Not much changes
    tried making a language switch button, did not work so had to delete.

---
+++ scss/javascript.js
@@ -0,0 +1,9 @@
+<script>
+    // Clicking the current language toggles the 'is-open' class 
+// on the 'lang' block:
+document.getElementById('lang-toggle').addEventListener('click', function(e){
+  e.preventDefault()}
+  $(this).parent().toggleClass('is-open');
+);
+
+</script>
